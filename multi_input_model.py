a39c2aaf76d50313b66173ac832c3a0611701da2
Tanxuecong <xuecong.tan@gmail.com>
2020-12-04 22:23:52 -0800

    put multi_input_model in separate script

--- multi_input_model.py
+++ multi_input_model.py
@@ -14,10 +14,9 @@ IS_SHUFFLE = False
 HIDDEN_STATE_VECTOR_DIM = 16
 EPOCHS = 40
 BATCHES = 100
-IS_ALL = True
-IS_HAT = False
-IS_OP = False
-# this implementation is for baseline LSTM model training
+
+# This model only work on data from processed_data/all
+# which contain both openpose and head orientation as feature
 def main():
     frame_sequences = np.load('LSTM_input.npy')
     labels = np.load('LSTM_labels.npy') #sequences labels not frame labels
@@ -29,37 +28,52 @@ def main():
     # normalize the rest feature between 0,1
     scaler_op = MinMaxScaler(feature_range=(-1,1))
     scaler_h = MinMaxScaler(feature_range=(0,1))
-    if IS_ALL:
-        for i in range(len(frame_sequences)):
-            frame_sequences[i][:,0:54] = scaler_op.fit_transform(frame_sequences[i][:,0:54])# normalize openpose 2d position
-            frame_sequences[i][:, 54:] = scaler_h.fit_transform(frame_sequences[i][:, 54:]) # normalize the rest feature
-    elif IS_HAT:
-        for i in range(len(frame_sequences)):
-            frame_sequences[i][:, 0:] = scaler_h.fit_transform(frame_sequences[i][:, 0:])  # normalize the rest feature
-    elif IS_OP:
-        for i in range(len(frame_sequences)):
-            frame_sequences[i][:, 0:] = scaler_op.fit_transform(frame_sequences[i][:, 0:])  # normalize openpose 2d position
+
+    for i in range(len(frame_sequences)):
+        frame_sequences[i][:,0:54] = scaler_op.fit_transform(frame_sequences[i][:,0:54])# normalize openpose 2d position
+        frame_sequences[i][:, 54:] = scaler_h.fit_transform(frame_sequences[i][:, 54:]) # normalize the rest feature
 
     # shuffle data if is shuffle
     if IS_SHUFFLE:
         frame_sequences, labels = shuffle(frame_sequences, labels)
 
-    model = Sequential()
-    # input_shape=(frame_sequences.shape[1:]) #(WINDOW_SIZE, num_features)
-    model.add(LSTM(HIDDEN_STATE_VECTOR_DIM, input_shape=(frame_sequences.shape[1:]), activation='relu', return_sequences=True))
-    model.add(Dropout(0.5))
+    # Extract feature input
+    openpose_input = []
+    head_ori_input = []
+    for i in range(len(frame_sequences)):
+        openpose_input.append(frame_sequences[i][:, 0:54])
+    openpose_input = np.array(openpose_input)
+    for i in range(len(frame_sequences)):
+        head_ori_input.append(frame_sequences[i][:, 54:])
+    head_ori_input = np.array(head_ori_input)
 
-    model.add(LSTM(HIDDEN_STATE_VECTOR_DIM, activation='relu'))
-    model.add(Dropout(0.5))
+    print(openpose_input.shape)
+    print(head_ori_input.shape)
+    inputA = Input(shape=(openpose_input.shape[1:]))
+    inputB = Input(shape=(head_ori_input.shape[1:]))
 
-    model.add(Dense(num_classes, activation='softmax'))
+    x = LSTM(HIDDEN_STATE_VECTOR_DIM, activation='relu')(inputA)
+    x = Dropout(0.5)(x)
+    x = Dense(8, activation="relu")(x)
+    x = Model(inputs=inputA, outputs=x)
 
-    model.compile(optimizer=Adam(lr=1e-4, decay=1e-5),
+    y = LSTM(HIDDEN_STATE_VECTOR_DIM, activation='relu')(inputB)
+    y = Dropout(0.5)(y)
+    y = Dense(8, activation="relu")(y)
+    y = Model(inputs=inputB, outputs=y)
+
+    combined = concatenate([x.output, y.output])
+
+    z = Dense(16, activation="relu")(combined)
+    z = Dense(num_classes, activation="softmax")(z)
+    model = Model(inputs=[x.input, y.input], outputs=z)
+
+    model.compile(optimizer=Adam(lr=1e-3, decay=1e-5),
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
     history = model.fit(
-        x=frame_sequences,
+        x=[openpose_input, head_ori_input],
         y=labels,
         validation_split=0.1,
         batch_size=BATCHES,
@@ -82,7 +96,6 @@ def main():
     plt.legend(['train', 'val'], loc='upper left')
     plt.show()
 
-
 if __name__ == '__main__':
     gpus = tf.config.experimental.list_physical_devices('GPU')
     # use a CUDA GPU it exits
@@ -95,4 +108,3 @@ if __name__ == '__main__':
             # Visible devices must be set before GPUs have been initialized
             print(e)
     main()
-
