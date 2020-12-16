a6a0f6f430cd98e23503c108add0eca6395a26a1
maxx <maxx@sfu.ca>
2020-12-16 01:49:41 -0800

    basic model

--- basic_model.py
+++ basic_model.py
@@ -14,7 +14,9 @@ from sklearn.svm import SVC, SVR
 import glob, os
 from sklearn.feature_selection import RFE
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
 
+# Plot the value distribution 
 def plotValueDist(y):
     y_count = y.groupby(['0']).size().tolist()
     labels = 'Confused', 'Uncertain', 'Not Confused'
@@ -25,6 +27,8 @@ def plotValueDist(y):
             shadow=True, startangle=90)
     ax1.axis('equal')
     plt.show()
+
+# Find the best parameters for svm
 def svmGridSearch(X,y):
     steps = [('scaler', StandardScaler()), ('SVM', SVC())]
     hyperF = {'SVM__C':np.logspace(-3, 2, 6), 'SVM__gamma':np.logspace(-3, 2, 6)}
@@ -35,6 +39,7 @@ def svmGridSearch(X,y):
     print(bestF.score(X, y))
     print(bestF.best_params_)
     
+# Find the best parameter for knn
 def knnGridSearch(X_train,y_train,X_valid,y_valid):
     n_neighbors = list(range(1,20))
     #Convert to dictionary
@@ -49,44 +54,55 @@ def knnGridSearch(X_train,y_train,X_valid,y_valid):
     #Print The value of best Hyperparameters
     print('Best n_neighbors:', best_model.best_estimator_.get_params()['n_neighbors'])
 
+def five_cv(model,X,y,name):
+    scores = cross_val_score(model, X, y, cv=5)
+    print(name + " 5-CV Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
 def main():
-    csv_path = './csv/alll/*'
+    csv_path = './processed_data/csv/all/*'
     df = pd.concat(map(pd.read_csv, glob.glob(os.path.join('',csv_path))))
     X, y = df.iloc[:,2:], df.iloc[:,1]
 
     X_train, X_valid, y_train, y_valid = train_test_split(X, y)
-    print(y)
+    print("Date was loaded...")
+    # print(y)
     # SVM 
-    # svm_all = make_pipeline(
-    # MinMaxScaler(),
-    # SVC(kernel='linear',C=10.0,gamma=0.01)
-    # )
-    # svm_all.fit(X_train, y_train)
-    # print("SVM accuracy: ", svm_all.score(X_valid, y_valid))
-
+    svm_all = make_pipeline(
+    MinMaxScaler(),
+    SVC(kernel='linear',C=10.0,gamma=0.01)
+    )
+    svm_all.fit(X_train, y_train)
+    print("SVM accuracy: ", svm_all.score(X_train, y_train))
+    print("SVM val accuracy: ", svm_all.score(X_valid, y_valid))
+    five_cv(svm_all,X,y, 'SVM')
     # KNN
     # knnGridSearch(X_train,y_train,X_valid,y_valid)
-    # knn_model = make_pipeline(
-    #     MinMaxScaler(),
-    #     KNeighborsClassifier(n_neighbors=2)
-    # )
-    # knn_model.fit(X_train, y_train)
-    # print("Knn accuracy: ", knn_model.score(X_valid, y_valid))
+    knn_model = make_pipeline(
+        MinMaxScaler(),
+        KNeighborsClassifier(n_neighbors=2)
+    )
+    knn_model.fit(X_train, y_train)
+    print("Knn accuracy: ", knn_model.score(X_train, y_train))
+    print("Knn val accuracy: ", knn_model.score(X_valid, y_valid))
+    five_cv(knn_model,X,y, 'KNN')
 
     rf = RandomForestClassifier(n_estimators=200,
         max_depth=9, min_samples_split=10, min_samples_leaf=10)
     rf.fit(X_train, y_train)
+    print("Random Forest accuracy: ", rf.score(X_train, y_train))
     print("Random Forest validation accuracy: ", rf.score(X_valid, y_valid))
-    # Plotting feature importance
-    features = X.columns
-    importances = rf.feature_importances_
-    indices = np.argsort(importances)
-    plt.figure(figsize=(25,8))
-    plt.title('Feature Importances')
-    plt.barh(range(len(indices)), importances[indices], color='b', align='center')
-    plt.yticks(range(len(indices)), [features[i] for i in indices])
-    plt.xlabel('Relative Importance')
-    plt.show()
+    five_cv(rf,X,y, 'Random Forest')
+
+    # # Plotting feature importance
+    # # features = X.columns
+    # # importances = rf.feature_importances_
+    # # indices = np.argsort(importances)
+    # # plt.figure(figsize=(25,8))
+    # # plt.title('Feature Importances')
+    # # plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+    # # plt.yticks(range(len(indices)), [features[i] for i in indices])
+    # # plt.xlabel('Relative Importance')
+    # # plt.show()
 
    
 
